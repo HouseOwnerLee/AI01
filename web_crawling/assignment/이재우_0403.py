e88ecfb2c6e1fad36784fcadd776eb6f2983902f
HouseOwnerLee <an3ppna@naver.com>
2026-04-04 19:35:52 +0900

    260404

--- web_crawling/assignment/이재우_0403.py
+++ web_crawling/assignment/이재우_0403.py
@@ -16,11 +16,14 @@ class MaxDateError(Exception):
         super().__init__("현재날짜 하루전까지 가능합니다.")
 
 def bugsCrawler(result):
+    # 시작 날짜 입력
     while True:
         try:
             date1 = datetime.strptime(input('시작 날짜를 입력해주세요: '), "%Y%m%d").date()
+            # 최소 날짜보다 작을 경우 에러
             if date1 < datetime(2006, 9, 22).date():
                 raise MinDateError()
+            # 오늘 날짜보다 크거나 같을 경우 에러
             if date1 >= datetime.today().date():
                 raise MaxDateError()
             break
@@ -29,19 +32,27 @@ def bugsCrawler(result):
     while True:
         try:
             date2 = datetime.strptime(input('끝 날짜를 입력해주세요: '), "%Y%m%d").date()
+            # 시작날짜보다 작을 경우 에러
             if date2 < date1:
                 raise Exception("시작 날짜 이후 날짜로 입력해주세요.")
+            # 오늘 날짜보다 크거나 같을 경우 에러
             if date2 >= datetime.today().date():
                 raise MaxDateError()
             break
         except Exception as e:
             print(e)
     print("-----------------------------------------------")
+
     while date1 <= date2:
-        bugsURL = 'https://music.bugs.co.kr/chart/track/day/total?%s' % date1.strftime("%Y%m%d")
+        # 일간차트 url
+        bugsURL = 'https://music.bugs.co.kr/chart/track/day/total?chartdate=%s' % date1.strftime("%Y%m%d")
+        # 웹페이지 소스
         html = urllib.request.urlopen(bugsURL)
+        # BeautifulSoup로 html 파싱
         soupBugs = BeautifulSoup(html, 'html.parser')
+        # 일간차트의 노래 목록영역
         tag_tbody = soupBugs.find('tbody')
+        # 노래를 한줄씩 순위, 가수, 제목 추출
         for song in tag_tbody.find_all('tr'):
             attrs = song.find_all('td')
             rank = attrs[1].find('strong').text
@@ -49,6 +60,7 @@ def bugsCrawler(result):
             title = song.find('th').find('a').text
             result.append([date1]+[rank]+[singer]+[title])
 
+        # 다음날
         date1 += timedelta(days=1)
     return
 
